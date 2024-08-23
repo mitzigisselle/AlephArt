6065241016001ff96feb0123f46e56e541e3a1af
Damaris <61764085+DamZav@users.noreply.github.com>
2024-08-22 18:11:22 -0600

    POST publicaciones (#27)
    Añado fecha de publicación y creo objeto publicaciones para que se vayan
    mostrando en lista y en orden cronológico.

--- JS publicaciones/publicaciones.js
+++ JS publicaciones/publicaciones.js
@@ -1,3 +1,7 @@
+
+let publicaciones = [];
+
+
 function crearPublicación() {
 
     // Valores inputs
@@ -6,54 +10,92 @@ function crearPublicación() {
     const imgUrl = document.getElementById('imgUrl').value;
     const audioUrl = document.getElementById('audioUrl').value;
     const videoUrl = document.getElementById('videoUrl').value;
+    const fechaCreacion = new Date();
 
     // Validación
     if (nombre === '' || contenido === '') {
-        alert('Ingresa nobre/contenido a la publicación');
+        alert('Campos vacíos. Plis ingresa nombre/contenido a la publicación');
         return;
     }
 
-    //Crea div
-    const publicacionDiv = document.createElement('div');
-    publicacionDiv.classList.add = 'publicacion';
+    // Crear objeto publicación
+    const nuevaPublicacion = {
+        nombre: nombre,
+        contenido: contenido,
+        imgUrl: imgUrl,
+        audioUrl: audioUrl,
+        videoUrl: videoUrl,
+        fechaCreacion: fechaCreacion
+    };
 
-    //Añade texto a nombre y publicación
-    nuevaPublicacion.innerHTML = `<h3>Publicación de ${nombre}</h3><p>${contenido}</p>`; 
+    // Añade a la lista  // Ordene cronológico
+    publicaciones.push(nuevaPublicacion);
+    publicaciones.sort((a, b) => b.fechaCreacion - a.fechaCreacion);
 
-    // Imagen
-    if (imgUrl) {
-        const img = document.createElement('img');
-        img.src = imgUrl;
-        img.alt = `Imagen de publicación`;
-        // img.style.maxWidth = '200px';  // pa ajustar el tamaño.
-        nuevaPublicacion.appendChild(img);
-    }
-
-    // Audio
-    if (audioUrl) {
-        const audio = document.createElement('audio');
-        audio.src = audioUrl;
-        // audio.controls = true; //controles de reprodcción
-        nuevaPublicacion.appendChild(audio);
-    }
-
-    // Video
-    if (videoUrl) {
-        const video = document.createElement('video');
-        video.src = videoUrl;
-        // video.controls = true; //controles de reprodcción
-        video.style.maxWidth = '300px';  // pa ajustar el tamaño.  
-        nuevaPublicacion.appendChild(video);
-    }
-
-    // Ponerlo en publicaciones
-    document.getelementById('publicaciones').appendChild(nuevaPublicacion);
+    // Llama a función de renderizado para actualizar y mostrr todas las publicaciones en el contenedor
+    renderPublicacion();
 
     // Limpia inputs  // Borra lo que estaba en los inputs al crear la publicación.
     document.getElementById('nombreUsuario').value = '';
     document.getElementById('contenidoPublicacion').value = '';
     document.getElementById('imgUrl').value = '';
     document.getElementById('audioUrl').value = '';
-    document.getElementById('videoUrl').value = '';    
+    document.getElementById('videoUrl').value = '';  
 
 }
+
+
+function renderPublicacion() {
+
+    // Limpia contenido anterior antes de renderización
+    const publicacionesContainer = document.getElementById('publicaciones');
+    publicacionesContainer.innerHTML = ''; 
+
+
+    // Iterar sobre el array de publicaciones para mosrtar. Para cada publicación se crea un nuevo div.
+    publicaciones.forEach((publicacion) => {
+        //Crea div
+        const publicacionDiv = document.createElement('div');
+        publicacionDiv.classList.add('publicacion');
+
+        // Añade texto (nombre, contenido, fechacreación
+        const fechaFormat = publicacion.fechaCreacion.toLocaleString(); // toLocaleString() pone por default la zona en la que estamos
+        publicacionDiv.innerHTML = `<h3>>Publicación de ${publicacion.nombre}</h3>
+                                    <p>${publicacion.contenido}</p>
+                                    <small>Publicado el: ${fechaFormat}</small>`;
+
+        // Imagen (si hay)
+        if (publicacion.imgUrl) {
+            const img = document.createElement('img');
+            img.src = publicacion.imgUrl;
+            // img.alt = `Imagen de publicación`;
+            img.style.maxWidth = '200px';  // pa ajustar el tamaño.
+            publicacionDiv.appendChild(img);
+        }
+
+        
+        // Audio (si hay)
+        if (publicacion.audioUrl) {
+            const audio = document.createElement('audio');
+            audio.src = publicacion.audioUrl;
+            // audio.controls = true; //controles de reprodcción
+            publicacionDiv.appendChild(audio);
+        }
+
+
+        // Video (si hay)
+        if (publicacion.videoUrl) {
+            const video = document.createElement('video');
+            video.src = publicacion.videoUrl;
+            // video.controls = true; //controles de reprodcción
+            video.style.maxWidth = '300px';  // pa ajustar el tamaño.  
+            publicacionDiv.appendChild(video);
+        }
+        
+        // Añade div de publicación al contenedor principal  // Agrega cada publicación a la lista en pantalla.
+        publicacionesContainer.appendChild(publicacionDiv);
+
+    });
+
+}
+
